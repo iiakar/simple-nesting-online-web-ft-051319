abefed4ea0af270dd0e2e7ab765eb0002b48a507
github username <ibsakar@gmail.com>
2019-10-16 11:37:18 -0400

    Done.

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-
+'require pry'
 def hopper
 	programmer_hash = 
  		{
@@ -15,8 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,8 +36,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
-
 def dennis_ritchies_language
 	programmer_hash = 
  		{
@@ -55,17 +54,19 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+ 
 
 	programmer_hash = 
  		{
+      :yukihiro_matsumoto => {
+        :known_for => "Ruby",
+         :languages => ["LISP", "C"]
+       },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -91,7 +92,7 @@ def changing_alan
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => "Object Orientation",
+          :known_for => "GUI",
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
@@ -100,7 +101,7 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+    #  alans_new_info = "GUI"
      
      
 end
@@ -118,7 +119,8 @@ def adding_to_dennis
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => ["C","Assembly"]
+          
         }
      }
 
